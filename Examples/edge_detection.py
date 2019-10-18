cc5733fc0bf4ee4dd873955e060806ff3120069a
Simon Brummer <simon.brummer.external@alliedvision.com>
2019-10-18 13:09:24 +0000

    Edge Detection Example
    git-svn-id: https://ahr-svn.avtnet.local/svn/host/Private/SBR/VimbaPython/trunk@27776 a56fe990-022f-c24f-abd5-7dd9c0b867e8

---
+++ Examples/edge_detection.py
@@ -0,0 +1,73 @@
+# TODO: Add License
+# TODO: Add Copywrite Note
+# TODA: Add Contact Info (clarify if this is required...)
+import ctypes
+import os
+import sys
+import time
+import cv2
+import numpy
+import threading
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from vimba import *
+
+class StreamHandler:
+    def __init__(self):
+        self.shutdown_event = threading.Event()
+
+    def __call__(self, cam: Camera, frame: Frame):
+        Log.get_instance().info('{} acquired {}'.format(cam, frame))
+
+        # Test abort condition
+        ESC_KEY_CODE = 27
+
+        key = cv2.waitKey(1)
+        if key == ESC_KEY_CODE:
+            self.shutdown_event.set()
+            return
+
+        # Apply Edge Detection on Frame
+        orig = frame.as_opencv_image()
+        edge = cv2.Canny(orig, 50, 100)
+        edge = edge[..., numpy.newaxis]
+
+        # Merge and show images
+        image = numpy.concatenate((orig, edge), axis=1)
+        cv2.imshow('Original, Edge Detect: ESC=Quit', image)
+
+        cam.queue_frame(frame)
+
+
+def main():
+    with Vimba.get_instance() as vimba:
+        # Lookup first detected camera.
+        cams = vimba.get_all_cameras()
+        if cams:
+            # Enter Camera context
+            with cams[0] as cam:
+
+                # Setup Camera
+                cam.get_feature_by_name('ExposureAuto').set('Continuous')
+                cam.get_feature_by_name('PixelFormat').set('Mono8')
+                cam.get_feature_by_name('Height').set(864)
+                cam.get_feature_by_name('Width').set(864)
+
+                # Enable Logging for capturing messages from the frame handler
+                vimba.enable_log(LOG_CONFIG_INFO_CONSOLE_ONLY)
+
+                stream_handler = StreamHandler()
+
+                # Start Streaming
+                cam.start_streaming(stream_handler)
+
+                stream_handler.shutdown_event.wait()
+
+                cam.stop_streaming()
+
+                vimba.disable_log()
+
+if __name__ == '__main__':
+    main()
+
--- Examples/sandbox.py
+++ Examples/sandbox.py
@@ -1,17 +1,21 @@
 # TODO: Add License
 # TODO: Add Copywrite Note
 # TODA: Add Contact Info (clarify if this is required...)
-
 import ctypes
 import os
 import sys
+import time
+import cv2
+import numpy
+import threading
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from vimba import *
 
 
 def main():
     pass
 
-
 if __name__ == '__main__':
     main()
-
