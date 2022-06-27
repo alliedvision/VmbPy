66cef366c405a30669d444c162efee823bc0b096
Niklas Kröger <Niklas.Kroeger@alliedvision.com>
2022-06-27 17:09:00 +0200

    Update test cases slightly
    This ensures, that interfaces/cameras are found by the transport layer
    and if not the relevant tests are skipped.
    
    Also changes the variable names in the test cases slightly to help with
    readability.

--- Tests/basic_tests/transport_layer_test.py
+++ Tests/basic_tests/transport_layer_test.py
@@ -25,47 +25,53 @@ class TransportLayerTest(VmbPyTestCase):
 
     def test_transport_layer_interfaces_have_correct_type(self):
         # Expectation: All interfaces reported by the transport layer are of type Interface
-        for i in self.vmb.get_all_transport_layers():
-            for j in i.get_interfaces():
-                self.assertIsInstance(j, Interface)
+        for tl in self.vmb.get_all_transport_layers():
+            interfaces = tl.get_interfaces()
+            if not interfaces:
+                self.skipTest(f'Could not test because {tl} did not provide any interfaces')
+            for i in interfaces:
+                self.assertIsInstance(i, Interface)
 
     def test_transport_layer_cameras_have_correct_type(self):
         # Expectation: All cameras reported by the transport layer are of type Camera
-        for i in self.vmb.get_all_transport_layers():
-            for j in i.get_cameras():
-                self.assertIsInstance(j, Camera)
+        for tl in self.vmb.get_all_transport_layers():
+            cameras = tl.get_cameras()
+            if not cameras:
+                self.skipTest(f'Could not test because {tl} did not provide any cameras')
+            for cam in cameras:
+                self.assertIsInstance(cam, Camera)
 
     def test_transport_layer_decode_id(self):
         # Expectation all transport layer ids can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_id(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_id(), '')
 
     def test_transport_layer_decode_name(self):
         # Expectation all transport layer names can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_name(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_name(), '')
 
     def test_transport_layer_decode_model_name(self):
         # Expectation all transport layer model names can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_model_name(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_model_name(), '')
 
     def test_transport_layer_decode_vendor(self):
         # Expectation all transport layer vendor fields can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_vendor(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_vendor(), '')
 
     def test_transport_layer_decode_version(self):
         # Expectation all transport layer version fields can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_version(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_version(), '')
 
     def test_transport_layer_decode_path(self):
         # Expectation all transport layer path fields can be decoded to something other than ''
-        for i in self.vmb.get_all_transport_layers():
-            self.assertNotEqual(i.get_path(), '')
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertNotEqual(tl.get_path(), '')
 
     def test_transport_layer_decode_type(self):
         # Expectation all transport layer types be in transport layer types
-        for i in self.vmb.get_all_transport_layers():
-            self.assertIn(i.get_type(), TransportLayerType)
+        for tl in self.vmb.get_all_transport_layers():
+            self.assertIn(tl.get_type(), TransportLayerType)
