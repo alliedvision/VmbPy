e2ae09232106a2586be3e18f17950ada934e9f92
Niklas Kröger <Niklas.Kroeger@alliedvision.com>
2022-09-20 14:36:23 +0200

    Use `Stream` module to access `GVSPAdjustPacketSize`
    The `GVSPAdjustPacketSize` feature is not part of the remote device
    (`Camera`). It is instead a feature of the `Stream` module.

--- Examples/asynchronous_grab.py
+++ Examples/asynchronous_grab.py
@@ -98,9 +98,10 @@ def setup_camera(cam: Camera):
     with cam:
         # Try to adjust GeV packet size. This Feature is only available for GigE - Cameras.
         try:
-            cam.GVSPAdjustPacketSize.run()
+            stream = cam.get_streams()[0]
+            stream.GVSPAdjustPacketSize.run()
 
-            while not cam.GVSPAdjustPacketSize.is_done():
+            while not stream.GVSPAdjustPacketSize.is_done():
                 pass
 
         except (AttributeError, VmbFeatureError):
