f084110074a22bce796896eea689d04d9e1de7eb
Niklas Kröger <Niklas.Kroeger@alliedvision.com>
2022-10-10 12:10:20 +0200

    Manually set `imageData` if it is not set by C-Level
    This assumption might not be correct in all situations (in fact it is
    probably only necessary to do this when no image data has been written
    to the buffer), but it prevents nullptr evaluations and therefore
    improves stability. The image that will be shown as result of this, is
    likey all 0-bytes, or random noise depending on if the memory is nulled
    during allocation or not.

--- vmbpy/frame.py
+++ vmbpy/frame.py
@@ -248,6 +248,10 @@ class Frame:
         result._frame.buffer = ctypes.cast(result._buffer, ctypes.c_void_p)
         result._frame.bufferSize = sizeof(result._buffer)
         # calculate offset of original imageData pointer into original buffer
+        if not self._frame.imageData:
+            # imageData pointer is not set. We assume image data begins at the start of the buffer
+            self._frame.imageData = ctypes.cast(ctypes.addressof(self._buffer),
+                                    ctypes.POINTER(VmbUint8))
         image_data_offset = ctypes.addressof(self._frame.imageData.contents) - ctypes.addressof(self._buffer)  # noqa 501: E501
         # set new imageData pointer to same offset into new buffer
         result._frame.imageData = ctypes.cast(ctypes.byref(result._buffer, image_data_offset),
@@ -528,6 +532,10 @@ class Frame:
         output_frame._frame.pixelFormat = target_fmt
 
         # calculate offset of original imageData pointer into original buffer
+        if not self._frame.imageData:
+            # imageData pointer is not set. We assume image data begins at the start of the buffer
+            self._frame.imageData = ctypes.cast(ctypes.addressof(self._buffer),
+                                    ctypes.POINTER(VmbUint8))
         image_data_offset = ctypes.addressof(self._frame.imageData.contents) - ctypes.addressof(self._buffer)   # noqa: E501
         # set new imageData pointer to same offset into new buffer
         output_frame._frame.imageData = ctypes.cast(ctypes.byref(output_frame._buffer, image_data_offset),      # noqa: E501
@@ -574,6 +582,10 @@ class Frame:
         # for this frame
         array_type = ctypes.c_uint8 * image_size
         # cast the imageData pointer to a suitable type to create a numpy array from it
+        if not self._frame.imageData:
+            # imageData pointer is not set. We assume image data begins at the start of the buffer
+            self._frame.imageData = ctypes.cast(ctypes.addressof(self._buffer),
+                                    ctypes.POINTER(VmbUint8))
         image_data = ctypes.cast(self._frame.imageData, ctypes.POINTER(array_type))
         return numpy.ndarray(shape=(height, width, channels_per_pixel),
                              buffer=image_data.contents,  # type: ignore
