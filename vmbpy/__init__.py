cf36e46a68348f5a1037bb51f6aa20f964a7f883
Florian Klostermann <Florian.Klostermann@alliedvision.com>
2024-03-19 10:39:04 +0100

    Increased VmbPy version to 1.0.5; Increased expected VmbC version to 1.0.6

--- vmbpy/__init__.py
+++ vmbpy/__init__.py
@@ -28,7 +28,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # Suppress 'imported but unused' - Error from static style checker.
 # flake8: noqa: F401
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 __all__ = [
     'VmbSystem',
